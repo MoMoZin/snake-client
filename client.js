a41a1da515a2f577174c50cc270de777b07d1c8d
kmomozin <khinmomozin@gmail.com>
2023-01-18 10:59:11 -0700

    naming my snake

--- client.js
+++ client.js
@@ -7,12 +7,17 @@ const connect = function() {
     port: 50541,
   });
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: MMZ");
+  });
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
+
 
   return conn;
 };
