614cd81c066a63d0b54492a8ab58a80617b0f400
kmomozin <khinmomozin@gmail.com>
2023-01-18 13:48:56 -0700

    added stdin and event listener

--- client.js
+++ client.js
@@ -13,12 +13,12 @@ const connect = function() {
 
     // moveUpSnake(conn);
 
-    slowMove(conn, "up", 50);
-    slowMove(conn, "up", 100);
-    slowMove(conn, "up", 150);
-    slowMove(conn, "left", 150);
-    slowMove(conn, "left", 200);
-    slowMove(conn, "down", 250);
+    // slowMove(conn, "up", 50);
+    // slowMove(conn, "up", 100);
+    // slowMove(conn, "up", 150);
+    // slowMove(conn, "left", 150);
+    // slowMove(conn, "left", 200);
+    // slowMove(conn, "down", 250);
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -31,14 +31,37 @@ const connect = function() {
   return conn;
 };
 
-const slowMove = (conn, move, time) => {
-  setTimeout(() => {
-    conn.write(`Move: ${move}`);
-  }, time);
+const handleUserInput = function(key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+  console.log('key:', key);
 };
+
+
+// setup interface to handle user input from stdin
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
+// const slowMove = (conn, move, time) => {
+//   setTimeout(() => {
+//     conn.write(`Move: ${move}`);
+//   }, time);
+// };
 // const moveUpSnake = (conn) => {
 //   setInterval(() => {
 //     conn.write("Move: up");
 //   }, 50);
 // };
+
+
 module.exports = { connect };
