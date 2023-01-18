f08265f7c01c0bc6cba320576c612d8ab999fe8e
kmomozin <khinmomozin@gmail.com>
2023-01-18 13:33:02 -0700

    playing with snake movements

--- client.js
+++ client.js
@@ -3,13 +3,22 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost",
+    host: "localhost",//"165.227.47.243",
     port: 50541,
   });
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: MMZ");
+
+    // moveUpSnake(conn);
+
+    slowMove(conn, "up", 50);
+    slowMove(conn, "up", 100);
+    slowMove(conn, "up", 150);
+    slowMove(conn, "left", 150);
+    slowMove(conn, "left", 200);
+    slowMove(conn, "down", 250);
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
@@ -22,4 +31,14 @@ const connect = function() {
   return conn;
 };
 
+const slowMove = (conn, move, time) => {
+  setTimeout(() => {
+    conn.write(`Move: ${move}`);
+  }, time);
+};
+// const moveUpSnake = (conn) => {
+//   setInterval(() => {
+//     conn.write("Move: up");
+//   }, 50);
+// };
 module.exports = { connect };
